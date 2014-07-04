ec5d1f077d0d990136eb4f6d61661841e2bc6efc
Samuele Santi <redshadow@hackzine.org>
2014-07-04 17:16:29 +0200

    Simple script to see example of logging

---
+++ .test-logger.py
@@ -0,0 +1,27 @@
+from nicelog.formatters import ColorLineFormatter
+import logging
+import sys
+
+logger = logging.getLogger('foo')
+logger.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler(sys.stderr)
+handler.setFormatter(ColorLineFormatter(show_date=True, show_function=True, show_filename=True))
+handler.setLevel(logging.DEBUG)
+
+logger.addHandler(handler)
+
+
+def do_stuff():
+    logger.debug('Debug message')
+    logger.info('Info message')
+    logger.warning('Warning message')
+    logger.error('Error message')
+    logger.critical('Critical message')
+    try:
+        raise ValueError("Example exception")
+    except:
+        logger.exception("An error happened")
+
+
+do_stuff()
