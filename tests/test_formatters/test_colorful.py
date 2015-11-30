74b9191d63d39b56cb4e8d56961d9fdb9794dbea
Samuele Santi <redshadow@hackzine.org>
2015-11-30 16:50:25 +0000

    Some tests

--- tests/test_formatters/test_colorful.py
+++ tests/test_formatters/test_colorful.py
@@ -4,13 +4,34 @@ from __future__ import absolute_import, division, unicode_literals
 
 from nicelog.formatters import Colorful
 from nicelog.colorers.terminal import Xterm16Colorer, Xterm256Colorer
+import logging
+import re
+
+import io
+from logging import getLogger, StreamHandler, DEBUG
+from freezegun import freeze_time
 
 
+@freeze_time('2015-11-30 16:30')
 class TestColorfulFormatter(object):
 
-    def test_debug_messages_are_rendered_properly(self):
+    def test_formatter_works_end_to_end(self):
+        outstream = io.StringIO()
+        handler = StreamHandler(outstream)
+        handler.setLevel(DEBUG)
         formatter = Colorful()
+        handler.setFormatter(formatter)
+        logger = getLogger('test_logger')
+        logger.setLevel(DEBUG)
+        logger.addHandler(handler)
 
-        msg = formatter.format()
-        pass
-    pass
+        logger.info('Hello, world')
+
+        data = outstream.getvalue()
+        assert '\x1b[' in data  # Make sure we have colors
+
+        clean = re.sub('\x1b\[.*?m', '', data)
+        assert clean == (
+            '   INFO    2015-11-30 16:30:00  test_logger  test_colorful.py:28 '
+            'test_colorful.test_formatter_works_end_to_end \n'
+            '    Hello, world\n')
